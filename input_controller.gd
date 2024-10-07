08a13bb04b8120821930a7c11217bce6fd0915ad
Stuart Olsen <stuart@sj-olsen.com>
2024-10-07 00:06:41 -0500

    Compute the camera transformation independently from the cube basis

--- input_controller.gd
+++ input_controller.gd
@@ -13,10 +13,10 @@ func _unhandled_input(event: InputEvent) -> void:
 		return
 
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		var cam_basis: Basis = %Camera3D.get_camera_transform().basis
-		var cube_basis: Basis = cam_basis.inverse() * %Cube.basis
 		var delta: Vector2 = event.screen_relative / 100.0
-		cube_basis = cube_basis.rotated(Vector3.RIGHT, delta.y)
-		cube_basis = cube_basis.rotated(Vector3.UP, delta.x)
-		%Cube.basis = cam_basis * cube_basis
+		var pitch = Basis(Vector3.RIGHT, delta.y)
+		var yaw = Basis(Vector3.UP, delta.x)
+		var camera = %Camera3D.get_camera_transform().basis
+		var trans = camera * yaw * pitch * camera.inverse()
+		%Cube.basis = trans * %Cube.basis
 		_viewport.set_input_as_handled()
